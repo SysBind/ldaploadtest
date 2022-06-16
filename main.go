41b06da0b789433132a7bd2d1ae5ab5ee49e6328
Asaf Ohayon <asaf@sysbind.co.il>
2022-06-16 14:05:11 +0300

    binding to LDAP from Go

--- main.go
+++ main.go
@@ -4,16 +4,33 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	ldap "github.com/go-ldap/ldap/v3"
 )
 
 func main() {
 	ldapURL := flag.String("u", "ldap://localhost:389", "LDAP Server URL")
+	cn := flag.String("c", "admin", "Common Name (i.e: login username)")
+	dn := flag.String("dn", "sysbind.test", "Distinguished Name (i.e: domain name)")
+	pass := flag.String("p", "p4ssw0rd", "Password")
+	flag.Parse()
+
 	l, err := ldap.DialURL(*ldapURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
-	fmt.Println("LDAP Load Testing Tool")
+
+	bindStr := fmt.Sprintf("cn=%s", *cn)
+	dcs := strings.Split(*dn, ".")
+	for i := range dcs {
+		bindStr = fmt.Sprintf("%s, dc=%s", bindStr, dcs[i])
+	}
+	fmt.Printf("attempting to connect using %s\n", bindStr)
+
+	err = l.Bind(bindStr, *pass)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
