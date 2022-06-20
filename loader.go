509dacfdf6b0661f5d09f3d33a779206c922ad04
Asaf Ohayon <asaf@sysbind.co.il>
2022-06-20 15:09:23 +0300

    playing with timings

--- loader.go
+++ loader.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"sync"
 	"time"
 )
 
-const startload = 100      // initial query per seconds
-const loadperiod = 5       // number of seconds to preserve current load
-const increment = 100      // the additional number of queries to add to the previous load
-const secondpart = 1 / 100 // hundredth of second
+const startload = 100 // initial query per seconds
+const loadperiod = 5  // number of seconds to preserve current load
+const increment = 100 // the additional number of queries to add to the previous load
 
 type Loader struct {
 	svc   *Service
@@ -23,27 +21,32 @@ func (loader *Loader) Run() error {
 	curperiod := 0
 	for {
 		gstart := time.Now()
-		var wg sync.WaitGroup
-		for i := 1; i < loader.reqps; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				usernum := rand.Intn(2000)
-				start := time.Now()
-				err := loader.svc.Query(fmt.Sprintf("demo%d", usernum))
-				elapsed := time.Since(start)
-				if elapsed.Milliseconds() > int64(1000) {
-					log.Fatalf("query exeeded 1 second at %d queries per second", i)
-				}
-				if err != nil {
-					log.Fatalf("query returned error at %d\n %v", i, err)
-				}
-			}()
+		for i := 0; i < 10; i++ {
+			//curstart := time.Now()
+			//var wg sync.WaitGroup
+			for j := 1; j < loader.reqps/10; j++ {
+				//wg.Add(1)
+				go func() {
+					//		defer wg.Done()
+					usernum := rand.Intn(2000)
+					start := time.Now()
+					err := loader.svc.Query(fmt.Sprintf("demo%d", usernum))
+					elapsed := time.Since(start)
+					if elapsed.Milliseconds() > int64(1000) {
+						log.Fatalf("query exeeded 1 second at %d queries per second", loader.reqps+j)
+					}
+					if err != nil {
+						log.Fatalf("query returned error at %d\n %v", j, err)
+					}
+					time.Sleep(10 * time.Millisecond)
+				}()
+			}
+			//wg.Wait()
+			//curelapsed := time.Since(curstart)
+			//log.Printf("elapsed %s at %d chunk %d", curelapsed, loader.reqps, i)
 		}
-		wg.Wait()
 		gelapsed := time.Since(gstart)
 		log.Printf("%d requests took %s", loader.reqps, gelapsed)
-
 		curperiod += 1
 		if curperiod == loadperiod {
 			curperiod = 0
